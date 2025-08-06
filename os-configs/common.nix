19767f1db9770450a5cca775a72d19c0ba7d75c2
David Villafaña <dvillafanaiv@proton.me>
2025-08-06 09:28:27 -0500

    feat: add resolved for wireguard compat

--- os-configs/common.nix
+++ os-configs/common.nix
@@ -25,6 +25,9 @@
   };
 
   networking.hostName = nodename; # Define your hostname.
+
+  services.resolved.enable = true;
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary

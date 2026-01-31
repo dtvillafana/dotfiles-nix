0a09c71c20159e58602620d4b8eaac6f63d98986
David Villafaña <dvillafanaiv@proton.me>
2026-01-30 20:49:15 -0600

    fix: add boot loader options

--- hardware-configs/rogdesktop.nix
+++ hardware-configs/rogdesktop.nix
@@ -19,6 +19,8 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/79f0df20-4791-4109-a638-a950ef824d71";
@@ -43,7 +45,6 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;

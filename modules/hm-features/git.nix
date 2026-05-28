1f3e3ad8945bb36bb0dd600ee8afd80cf3d85147
dtvillafana <82293276+dtvillafana@users.noreply.github.com>
2026-05-28 12:32:18 -0500

    feat: add codeberg

--- modules/hm-features/git.nix
+++ modules/hm-features/git.nix
@@ -15,8 +15,8 @@
             "store --file=${osConfig.users.users.vir.home}/.git-credentials-github";
           credential."https://gitlab.com".helper =
             "store --file=${osConfig.users.users.vir.home}/.git-credentials-gitlab";
-          credential."https://bitbucket.org".helper =
-            "store --file=${osConfig.users.users.vir.home}/.git-credentials-bitbucket";
+          credential."https://codeberg.org".helper =
+            "store --file=${osConfig.users.users.vir.home}/.git-credentials-codeberg";
         };
         lfs.enable = true;
       };

b9e48739dd55e5b6b1f2581599da9364dbdaa024
David Villafaña <david.villafana@capcu.org>
2026-07-28 13:13:58 -0500

    fix: bootstrap

--- modules/common/bootstrap.nix
+++ modules/common/bootstrap.nix
@@ -37,6 +37,10 @@
 
       services.printing.enable = true;
       services.desktopManager.plasma6.enable = true;
+      services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
 
       documentation.nixos.enable = false;
       services.pulseaudio.enable = false;
